8c0d605239c8be65c7335966768d88f9581e24a6
ftacm <137812990+ftacm@users.noreply.github.com>
2024-09-24 04:56:07 +0000

    Aventureiro codado e comentado + habilidade cruz

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,22 +3,100 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // Gera o tabuleiro com 0 em todas as casas
+    int tabuleiro[10][10];
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
+    //imprime o tabuleiro gerado vazio
+    printf("Tabuleiro Vazio:\n");
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nPosicionando navio vertical:\n");
+    // Escolhe um ponto horizontal único para colocar o navio e percorre 3 casas na vertical
+    for (int i=2;i<5;i++){
+        for (int j=0;j<10;j++){
+            tabuleiro[i][1] = 3;
+        }
+    }
+    // Imprime o tabuleiro com as novas alterações
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+     printf("\nPosicionando navio horizontal:\n");
+     // Escolhe um ponto vertical único para colocar o navio e percorre 3 casas na horizontal
+    for (int i=0;i<10;i++){
+        for (int j=1;j<4;j++){
+            tabuleiro[0][j] = 3;
+        }
+    }
+    // Imprime o tabuleiro com as novas alterações
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nPosicionando navios na diagonal:\n");
+    // Escolhe um quadrado com 9 casas para ser preenchido, 3 a 5 e 6 a 8
+    for (int i=3;i<6;i++){
+        for (int j=6;j<9;j++){
+            // Condição if para preencher com 3 se e somente se a casa coincidir com o preenchimento diagonal, i sempre sendo j-3
+            if (i=j-3){
+            tabuleiro[i][j] = 3;
+            } else {
+                tabuleiro[i][j] = 0;
+            }
+        }
+    }
+    // Escolhe um quadrado com 9 casas para ser preenchido, 5 a 7 e 6 a 8
+    for (int i=5;i<8;i++){
+        for (int j=6;j<9;j++){
+            // Condição if para preencher com 3 se e somente se a casa coincidir com o preenchimento diagonal, i sempre sendo j-1
+            if (i=j-1){
+            tabuleiro[i][j] = 3;
+            } else {
+                tabuleiro[i][j] = 0;
+            }
+        }
+    }
+    //imprime o tabuleiro com os navios na diagonal
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nAcionando Habilidade Cruz:\n");
+    // Gera o número 1 em todas as casas verticais na altura 5 horizontal
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            tabuleiro[i][5] = 1;
+        }
+    }
+    // Gera o número 1 em todas as casas horizontais na altura 4 vertical
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            tabuleiro[4][j] = 1;
+        }
+    }
+    // Imprime o tabuleiro com as alterações
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     // Exemplos de exibição das habilidades:
     // Exemplo para habilidade em cone:
